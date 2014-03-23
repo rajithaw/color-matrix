b6289a4db66d6d5f63d517bedea8a3cef4147114
Rajitha Wimalasooriya <rajithaw@hotmail.com>
2014-03-23 23:22:04 +1100

    Modified assembly info

--- Rtw.ColorMatrix/Properties/AssemblyInfo.cs
+++ Rtw.ColorMatrix/Properties/AssemblyInfo.cs
@@ -1,21 +1,27 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-//
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//
 [assembly: AssemblyTitle("Rtw.ColorMatrix")]
 [assembly: AssemblyDescription("General purpose color matrix class")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyProduct("Rtw.ColorMatrix")]
 [assembly: AssemblyCopyright("Rajitha Wimalasooriya")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
+[assembly: AssemblyCulture("")]
 
-//
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99BA147E-AE14-4B83-B41F-85317DC5109C")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -23,12 +29,12 @@ using System.Runtime.CompilerServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyFileVersion("0.8.*")]
 
-[assembly: AssemblyVersion("0.5.*")]
-
-//
 // In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
 //
@@ -52,7 +58,6 @@ using System.Runtime.CompilerServices;
 //       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
 //   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
 //       documentation for more information on this.
-//
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
